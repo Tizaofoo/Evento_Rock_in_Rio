27a8bdac1c1e072f5a48035babcb0be08f60a30f
Tiago Troquato de Oliveira <158060634+Tizaofoo@users.noreply.github.com>
2024-06-26 14:54:36 -0300

    criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
-console.log('ola mundo');
+AOS.init();
+
+dataDoEvento = new Date("Sep 13, 14, 15, 19, 20, 21 e 22 2024");
+const timeStampDoEvento = dataDoEvento.gettime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date ();
+    const timestampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timestampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+
+    console.log(diasAteOEvento);
+    console.log(horasAteOEvento);
+    console.log(minutosAteOEvento);
+    console.log(segundosAteOEvento);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+}, 1000);
