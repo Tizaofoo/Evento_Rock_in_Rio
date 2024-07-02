d718af2732cdbf54d46ce9daadc3d14f67ecc551
Tiago Troquato de Oliveira <158060634+Tizaofoo@users.noreply.github.com>
2024-07-01 23:11:33 -0300

    final projeto

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 AOS.init();
 
-dataDoEvento = new Date("Sep 13, 2024 19:00:00");
-const timeStampDoEvento = dataDoEvento.gettime();
+dataDoEvento = new Date('Sep 13, 2024 19:00:00');
+const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function() {
     const agora = new Date ();
@@ -9,14 +9,20 @@ const contaAsHoras = setInterval(function() {
 
     const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
 
-    const diaEmMs = 1000 * 60 * 60 * 24;
-    const horaEmMs = 1000 * 60 * 60;
-    const minutoEmMs = 1000 * 60;
+    const diaForMs = 1000 * 60 * 60 * 24;
+    const horaForMs = 1000 * 60 * 60;
+    const minutoForMs = 1000 * 60;
 
-    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
-    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
-    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
-    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaForMs);
+    const horasAteOEvento = Math.floor(distanciaAteOEvento % diaForMs / horaForMs);
+    const minutosAteOEvento = Math.floor(distanciaAteOEvento % horaForMs / minutoForMs);
+    const segundosAteOEvento = Math.floor(distanciaAteOEvento % minutoForMs / 1000);
+
+    /*console.log(diasAteOEvento);
+console.log(horasAteOEvento);
+console.log(minutosAteOEvento);
+console.log(segundosAteOEvento);
+*/
 
     document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
 
